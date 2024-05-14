9e19b3eee5563f23af07674ea9898ea1cc451716
Matias Ambrosi <matiambrosi@gmail.com>
2024-05-14 06:00:02 -0300

    bottom sheet added with search bar but no input text

---
+++ app/src/main/java/com/gfreeman/takeme/login/view/map/MapFragment.kt
@@ -0,0 +1,45 @@
+package com.gfreeman.takeme.login.view.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gfreeman.takeme.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+class MapFragment : Fragment() {
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+    //private lateinit var placesSearchView: CustomSearchView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+        val bottomSheet = view.findViewById<View>(R.id.map_bottom_sheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.isHideable = false
+        bottomSheetBehavior.peekHeight = resources.getDimensionPixelSize(com.google.android.material.R.dimen.design_bottom_sheet_peek_height_min)
+        return view
+    }
+
+    /*companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MapFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MapFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }*/
+    }
--- app/src/main/java/home/view/HomeActivity.kt
+++ app/src/main/java/home/view/HomeActivity.kt
@@ -6,37 +6,53 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.gfreeman.takeme.R
+import com.gfreeman.takeme.login.view.map.MapFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var mapFragment: Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
         configureViews()
     }
     private fun configureViews(){
-        val mainTitle = findViewById<TextView>(R.id.txt_test)
+
+        mapFragment = MapFragment()
+        loadFragment(mapFragment)
         val bottomNavbarView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavbarView.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.nav_home_item1 -> {
                     // Respond to navigation item 1 click
-                    mainTitle.text = "Favorites page"
+
                     true
                 }
                 R.id.nav_home_item2 -> {
-                    mainTitle.text = "Map page"
+                    loadFragment(mapFragment)
                     true
                 }
                 R.id.nav_home_item3 -> {
-                    mainTitle.text = "Profile page"
                     true
                 }
                 else -> false
             }
         }
     }
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.home_container, fragment)
+        transaction.commit()
+    }
+
+
 }
