ed9b718420cd68303181dae9e6be43a8a9270fad
Matias Ambrosi <matiambrosi@gmail.com>
2024-05-14 04:14:09 -0300

    Bottom nav btns working with name and icons

--- app/src/main/java/home/view/HomeActivity.kt
+++ app/src/main/java/home/view/HomeActivity.kt
@@ -1,21 +1,42 @@
 package home.view
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gfreeman.takeme.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home_container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        configureViews()
+    }
+    private fun configureViews(){
+        val mainTitle = findViewById<TextView>(R.id.txt_test)
+        val bottomNavbarView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavbarView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.nav_home_item1 -> {
+                    // Respond to navigation item 1 click
+                    mainTitle.text = "Favorites page"
+                    true
+                }
+                R.id.nav_home_item2 -> {
+                    mainTitle.text = "Map page"
+                    true
+                }
+                R.id.nav_home_item3 -> {
+                    mainTitle.text = "Profile page"
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
